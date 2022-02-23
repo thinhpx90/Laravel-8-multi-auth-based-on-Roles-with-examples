8178d0a0753dea8094779b3bc78b926d5afde153
Gia Thinh <thinhphuongxa1@gmail.com>
2022-02-23 23:33:17 +0700

    Cau 12 + Example DataBase

--- _db/laravel_8_multiauth.sql
+++ _db/laravel_8_multiauth.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 12, 2021 at 01:15 PM
--- Server version: 10.4.8-MariaDB
--- PHP Version: 7.3.10
+-- Host: localhost:3306
+-- Generation Time: Feb 23, 2022 at 03:54 PM
+-- Server version: 5.7.33
+-- PHP Version: 7.4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,6 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `experiences`
+--
+
+CREATE TABLE `experiences` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `job_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `start_time` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `end_time` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `company_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `experiences`
+--
+
+INSERT INTO `experiences` (`id`, `job_name`, `start_time`, `end_time`, `company_name`, `description`, `created_at`, `updated_at`) VALUES
+(7, 'Công việc 2', 'Công việcbbbb', 'Công việc 2', 'Công việc 2', 'Công việc 2', '2022-02-23 02:30:02', '2022-02-23 07:20:12'),
+(9, 'Công việc 9', 'Công việssssss', 'Công việc 9', 'Công việc 9', 'Công việc 9', '2022-02-23 06:53:17', '2022-02-23 06:53:27');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `failed_jobs`
 --
 
@@ -35,8 +59,31 @@ CREATE TABLE `failed_jobs` (
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file_path` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `files`
+--
+
+INSERT INTO `files` (`id`, `name`, `file_path`, `created_at`, `updated_at`) VALUES
+(1, '1645626401_17052021.docx', '/storage/uploads/1645626401_17052021.docx', '2022-02-23 07:26:41', '2022-02-23 07:26:41'),
+(2, '1645626678_17052021.docx', '/storage/uploads/1645626678_17052021.docx', '2022-02-23 07:31:18', '2022-02-23 07:31:18'),
+(3, '1645627102_AnhTesst.jpg', '/storage/uploads/1645627102_AnhTesst.jpg', '2022-02-23 07:38:22', '2022-02-23 07:38:22');
 
 -- --------------------------------------------------------
 
@@ -55,9 +102,11 @@ CREATE TABLE `migrations` (
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1);
+(5, '2014_10_12_000000_create_users_table', 1),
+(6, '2014_10_12_100000_create_password_resets_table', 1),
+(7, '2019_08_19_000000_create_failed_jobs_table', 1),
+(8, '2022_02_22_171216_create_experiences_table', 1),
+(9, '2022_02_23_141121_create_files_table', 2);
 
 -- --------------------------------------------------------
 
@@ -79,29 +128,35 @@ CREATE TABLE `password_resets` (
 
 CREATE TABLE `users` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `role` tinyint(4) DEFAULT NULL,
-  `favoriteColor` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `picture` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `favoriteColor` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `picture` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `role`, `favoriteColor`, `picture`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(2, 'Paul Julius Oleac', 'admin@gmail.com', 1, 'Red', 'UIMG_20210502608ea5b926248.jpg', NULL, '$2y$10$2Lp1RkhXSmcRkhpydFg2D.OcWNJ3S/uHFxzYhv1D7rTvgSbA/rmmi', NULL, '2021-04-12 12:52:31', '2021-05-02 12:00:14'),
-(15, 'User', 'user@gmail.com', 2, 'blue', '293101621939201_avatar.png', NULL, '$2y$10$cM903OrlVnXu.W5NDz7QQeQ.H.L1dDlAnOE1Ky0KdSU5y7r43OkcS', NULL, '2021-05-25 08:40:01', '2021-05-25 08:40:01');
+(2, 'Paul Julius Oleac', 'admin@gmail.com', 1, 'Red', 'UIMG_20210502608ea5b926248.jpg', NULL, '$2a$12$80pH9NSCYxwXZCZAII7PZuGAEISz435ZtUm0nv9b9BkjUG5n2YcD2', NULL, '2021-04-12 05:52:31', '2021-05-02 05:00:14'),
+(15, 'User', 'user@gmail.com', 2, 'blue', '293101621939201_avatar.png', NULL, '$2y$10$cM903OrlVnXu.W5NDz7QQeQ.H.L1dDlAnOE1Ky0KdSU5y7r43OkcS', NULL, '2021-05-25 01:40:01', '2021-05-25 01:40:01');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `experiences`
+--
+ALTER TABLE `experiences`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `failed_jobs`
@@ -109,6 +164,12 @@ INSERT INTO `users` (`id`, `name`, `email`, `role`, `favoriteColor`, `picture`, 
 ALTER TABLE `failed_jobs`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -126,12 +187,17 @@ ALTER TABLE `password_resets`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `experiences`
+--
+ALTER TABLE `experiences`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -140,16 +206,16 @@ ALTER TABLE `failed_jobs`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
